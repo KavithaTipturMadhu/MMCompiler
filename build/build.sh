a57f6122d49f383241663f699a1eef6d95c65c4d
Anup Kini <anup.kini@morphing.in>
2016-05-25 09:33:04 +0530

    added build dir & script.

---
+++ build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#file=./Makefile
+#
+#if [-f "$file"]
+#then
+#	make -j4 CXXFLAGS="-I $PWD/../ -L $PWD/../lpsolve"
+#else
+	../configure --prefix=$PWD/../
+	make -j4 CXXFLAGS="-I $PWD/../ -L $PWD/../lpsolve"
+#fi
