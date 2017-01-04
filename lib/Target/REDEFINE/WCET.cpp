0a93883e5df694fdf35a137e31adba31712512e4
kavitha <kavitha@cadl.iisc.ernet.in>
2017-01-04 15:50:20 +0530

    WCET files commit

---
+++ lib/Target/REDEFINE/WCET.cpp
@@ -0,0 +1,18 @@
+/*
+ * WCET.cpp
+ *
+ *  Created on: 04-Jan-2017
+ *      Author: kavitha
+ */
+
+#include "WCET.h"
+
+WCET::WCET() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WCET::~WCET() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ lib/Target/REDEFINE/WCET.h
@@ -0,0 +1,17 @@
+/*
+ * WCET.h
+ *
+ *  Created on: 04-Jan-2017
+ *      Author: kavitha
+ */
+
+#ifndef LIB_TARGET_REDEFINE_WCET_H_
+#define LIB_TARGET_REDEFINE_WCET_H_
+
+class WCET {
+public:
+	WCET();
+	virtual ~WCET();
+};
+
+#endif /* LIB_TARGET_REDEFINE_WCET_H_ */
