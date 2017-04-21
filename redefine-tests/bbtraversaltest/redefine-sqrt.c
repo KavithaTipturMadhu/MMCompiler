e92dbf27477d08be6c2f6b03775df3f24c3a3812
kavitha <kavitha@cadl.iisc.ernet.in>
2017-04-21 19:06:24 +0530

    a failing test case added to fix in the next commit

---
+++ redefine-tests/bbtraversaltest/redefine-sqrt.c
@@ -0,0 +1,87 @@
+/*************************************************************************/
+/*                                                                       */
+/*   SNU-RT Benchmark Suite for Worst Case Timing Analysis               */
+/*   =====================================================               */
+/*                              Collected and Modified by S.-S. Lim      */
+/*                                           sslim@archi.snu.ac.kr       */
+/*                                         Real-Time Research Group      */
+/*                                        Seoul National University      */
+/*                                                                       */
+/*                                                                       */
+/*        < Features > - restrictions for our experimental environment   */
+/*                                                                       */
+/*          1. Completely structured.                                    */
+/*               - There are no unconditional jumps.                     */
+/*               - There are no exit from loop bodies.                   */
+/*                 (There are no 'break' or 'return' in loop bodies)     */
+/*          2. No 'switch' statements.                                   */
+/*          3. No 'do..while' statements.                                */
+/*          4. Expressions are restricted.                               */
+/*               - There are no multiple expressions joined by 'or',     */
+/*                'and' operations.                                      */
+/*          5. No library calls.                                         */
+/*               - All the functions needed are implemented in the       */
+/*                 source file.                                          */
+/*                                                                       */
+/*                                                                       */
+/*************************************************************************/
+/*                                                                       */
+/*  FILE: sqrt.c                                                         */
+/*  SOURCE : Public Domain Code                                          */
+/*                                                                       */
+/*  DESCRIPTION :                                                        */
+/*                                                                       */
+/*     Square root function implemented by Taylor series.                */
+/*                                                                       */
+/*  REMARK :                                                             */
+/*                                                                       */
+/*  EXECUTION TIME :                                                     */
+/*                                                                       */
+/*                                                                       */
+/*************************************************************************/
+
+/*
+float fabs(float n)
+{
+  float f;
+
+  if (n >= 0) f = n;
+  else f = -n;
+  return f;
+}
+*/
+
+float sqrt(float val)
+{
+  float x = val/10;
+
+  float dx;
+
+  float diff;
+  float min_tol = 0.00001f;
+
+  int i, flag;
+
+  flag = 0;
+  if (val == 0 ) x = 0;
+  else {
+    for (i=1;i<20;i++)
+      {
+	if (!flag) {
+	  dx = (val - (x*x)) / (2.0f * x);
+	  x = x + dx;
+	  diff = val - (x*x);
+	  if (diff <= min_tol) flag = 0;
+	}
+	else 
+	  x = x ;
+      }
+  }
+  return (x);
+}
+
+void redefine_start(void)
+{
+  float x=1234.0f;
+  sqrt(x);
+}
