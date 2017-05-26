39d3f92dc4c2481dd3aed9f470452dab12f7e3c9
kavitha <kavitha@cadl.iisc.ernet.in>
2017-05-26 20:59:03 +0530

    test case for loop thats screwing up

---
+++ redefine-tests/fib-1cr/loop.c
@@ -0,0 +1,13 @@
+void redefine_start(){
+	int i, j, k, l;
+	int a[10];
+	for(l=0;l<10;l++){
+		for(k=0;k<10;k++){
+			for(i=0;i<10;i++){
+				for(j=0;j<10;j++){
+					a[i]=a[i];
+				}
+			}
+		}	
+	}
+}
