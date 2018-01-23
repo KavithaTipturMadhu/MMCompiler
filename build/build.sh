a528dd409b876fe7c3cf38b3ed7ee60e0e2a3011
Tarun <cmakeshmail@gmail.com>
2018-01-23 15:34:47 +0530

    inprogress - compilation successful

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,6 @@
 #then
 #	make -j4 CXXFLAGS="-I $PWD/../ -L $PWD/../lpsolve"
 #else
-	../configure --prefix=$PWD/../
-	make -j4 CXXFLAGS="-I $PWD/../ -L $PWD/../lpsolve -frtti"
+# ../configure --prefix=$PWD/../
+	make -j4 CXXFLAGS="-std=c++11 -I $PWD/../ -L $PWD/../lpsolve -frtti"
 #fi
