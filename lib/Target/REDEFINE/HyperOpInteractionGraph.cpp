402875b8420bc58311d2ed19f8bbc74debe1c438
Kavitha Madhu <kavithamadhu@Kavithas-MacBook-Air.local>
2019-01-16 11:10:14 +0530

    fixes: rfalloc, all dynamic hops, cleanup

--- lib/Target/REDEFINE/HyperOpInteractionGraph.cpp
+++ lib/Target/REDEFINE/HyperOpInteractionGraph.cpp
@@ -3750,6 +3750,7 @@ void HyperOpInteractionGraph::print(raw_ostream &os, int debug) {
 	os << "}\n";
 }
 
+<<<<<<< HEAD
 void setUpdatedMetadata(HyperOp* nodeForRemoval, Instruction** instr, StringRef mdkind) {
 	vector<Value*> updatedConsumerList;
 	MDNode* consumedByMDNode = (*instr)->getMetadata(mdkind);
@@ -3839,4 +3840,36 @@ void HyperOpInteractionGraph::removeUnreachableHops() {
 		}
 	}
 
+=======
+void HyperOpInteractionGraph::removeUnreachableHops(){
+	//This had to be written as follows because removal of one node may cause other nodes to go hanging
+		while (true) {
+			bool updatedGraph = false;
+			list<HyperOp*> vertices = this->Vertices;
+			for (list<HyperOp*>::iterator vertexItr = vertices.begin(); vertexItr != vertices.end(); vertexItr++) {
+				if (!(*vertexItr)->isEndHyperOp() && (*vertexItr)->ChildMap.empty()) {
+					if (!(*vertexItr)->isUnrolledInstance()) {
+						(*vertexItr)->getFunction()->eraseFromParent();
+					}
+					this->removeHyperOp(*vertexItr);
+					updatedGraph = true;
+					break;
+				}
+
+				else if (!(*vertexItr)->isStartHyperOp() && (*vertexItr)->ParentMap.empty()) {
+					if (!(*vertexItr)->isUnrolledInstance()) {
+						(*vertexItr)->getFunction()->eraseFromParent();
+					}
+					this->removeHyperOp(*vertexItr);
+					updatedGraph = true;
+					break;
+				}
+			}
+			if (!updatedGraph) {
+				break;
+			}
+		}
+
+		this->updateLocalRefEdgeMemOffset();
+>>>>>>> fixes: rfalloc, all dynamic hops, cleanup
 }
