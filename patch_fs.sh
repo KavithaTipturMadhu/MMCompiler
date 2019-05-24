0dfbeafe4182d447cc0743b0fb1cdf617be58e41
harish <harish.morphing.in>
2019-05-24 11:58:14 +0530

    patch changes for mac

--- patch_fs.sh
+++ patch_fs.sh
@@ -1,2 +1,10 @@
 sed -i.orig '1d' $1
-sed -i --  "s/\bfs\b/`tail -n 1 $1 | cut -c7-50`/" $1
+darwin=false;
+case "$(uname)" in
+    Darwin*) darwin=true ;;
+esac
+if $darwin; then
+    sed -i -- "s/[[:<:]]fs[[:>:]]/`tail -n 1 $1 | cut -c7-50`/" $1
+else
+    sed -i -- "s/\bfs\b/`tail -n 1 $1 | cut -c7-50`/" $1
+fi
